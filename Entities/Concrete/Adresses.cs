e669cc7c932b64092098c007f34c712e12d9705c
ahmet özcan <f111216079@ktun.edu.tr>
2021-04-19 23:03:45 +0300

    Address database class added

---
+++ Entities/Concrete/Adresses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Adresses
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CityId { get; set; }
+        public char AddressDetail { get; set; }
+        public char PostalCode { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
