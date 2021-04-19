47603daf34fd9504207d1a866145ba56c1a613ad
ahmet özcan <f111216079@ktun.edu.tr>
2021-04-19 23:11:30 +0300

    IEntity Correction

--- Entities/Concrete/Adresses.cs
+++ Entities/Concrete/Adresses.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Adresses
+    public class Adresses : IEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
--- Entities/Concrete/Baskets.cs
+++ Entities/Concrete/Baskets.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Baskets
+    public class Baskets :IEntity
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
